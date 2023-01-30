aadeaff38d6a9c77f5c061713b09bf942c6d43d9
mehakgupta921 <mehakgupta00921@gmail.com>
2023-01-30 22:10:38 +0530

    Email Commit

---
+++ accounts/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('' ,  home  , name="home"),
+    path('register' , register_attempt , name="register_attempt"),
+    path('login' , login_attempt , name="login_attempt"),
+    path('token' , token_send , name="token_send"),
+    path('success' , success , name='success'),
+    path('verify/<auth_token>',verify,name="verify"),
+    path('error',error_page,name="error")
+
+    
+
+    
+   
+]
